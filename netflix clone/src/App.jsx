f5d366bb16733355a8e9bc03feebcb3131997ae1
Abhishek Kumar <abhishekkumar224497@gmail.com>
2025-12-26 21:39:50 +0530

    Add Netlify SPA redirects

--- netflix clone/src/App.jsx
+++ netflix clone/src/App.jsx
@@ -1,49 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Resume from "./pages/Resume";
 import Skills from "./pages/Skills";
 import Markets from "./pages/Markets";
 import Testimonials from "./pages/Testimonials";
 import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
 
-// Resume
-import ResumeHome from "./pages/Resume/ResumeHome";
-import DubaiResume from "./pages/Resume/Dubai";
-import SingaporeResume from "./pages/Resume/Singapore";
-import MalaysiaResume from "./pages/Resume/Malaysia";
-
-// Case Studies
-import SEO from "./pages/CaseStudies/SEO";
-import Graphics from "./pages/CaseStudies/Graphics"; 
-import WebProjects from "./pages/CaseStudies/WebProjects";
-
-function App() {
+export default function App() {
   return (
-    <Router>
+    <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/markets" element={<Markets />} />
         <Route path="/testimonials" element={<Testimonials />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
-
-        {/* Resume */}
-        <Route path="/resume" element={<ResumeHome />} />
-        <Route path="/resume/dubai" element={<DubaiResume />} />
-        <Route path="/resume/singapore" element={<SingaporeResume />} />
-        <Route path="/resume/malaysia" element={<MalaysiaResume />} />
-
-        {/* Case Studies */}
-        <Route path="/case-studies/seo" element={<SEO />} />
-        <Route path="/case-studies/graphics" element={<Graphics />} /> 
-        <Route path="/case-studies/web-projects" element={<WebProjects />} />
       </Routes>
-    </Router>
+    </>
   );
 }
-
-export default App;
--- netflix clone/src/components/Navbar.jsx
+++ netflix clone/src/components/Navbar.jsx
@@ -1,70 +1,22 @@
-import { Link, NavLink } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
-  const [show, setShow] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
-
-  // Hide / show navbar on scroll (Netflix style)
-  useEffect(() => {
-    const controlNavbar = () => {
-      if (window.scrollY > lastScrollY) {
-        setShow(false);
-      } else {
-        setShow(true);
-      }
-      setLastScrollY(window.scrollY);
-    };
-
-    window.addEventListener("scroll", controlNavbar);
-    return () => window.removeEventListener("scroll", controlNavbar);
-  }, [lastScrollY]);
-
   return (
-    <div className={`navbar ${show ? "show" : "hide"}`}>
-      
-      {/* Logo */}
-      <Link to="/" className="logo">
-        NETFOLIO
-      </Link>
+    <div className="navbar">
+      <h1 className="logo">NETFLIX</h1>
 
-      {/* Main Menu */}
-      <div className="menu">
-        <NavLink to="/" className="nav-link">Home</NavLink>
-        <NavLink to="/about" className="nav-link">About</NavLink>
-        <NavLink to="/resume" className="nav-link">Resume</NavLink>
-        <NavLink to="/skills" className="nav-link">Skills</NavLink>
-        <NavLink to="/markets" className="nav-link">Markets</NavLink>
-
-        {/* Case Studies Dropdown */}
-        <div className="dropdown">
-          <span className="nav-link">Case Studies</span>
-          <div className="dropdown-menu">
-            <Link to="/case-studies/seo">SEO</Link>
-            <Link to="/case-studies/paid-ads">Paid Ads</Link>
-            <Link to="/case-studies/web-projects">Web Projects</Link>
-          </div>
-        </div>
-
-        <NavLink to="/contact" className="nav-link">Contact</NavLink>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/resume">Resume</Link>
+        <Link to="/skills">Skills</Link>
+        <Link to="/markets">Markets</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/contact">Contact</Link>
       </div>
 
-      {/* Profile Dropdown */}
-      <div className="dropdown profile-dropdown">
-        <span className="profile">Abhishek ⌄</span>
-        <div className="dropdown-menu right">
-          <Link to="/resume">Resume</Link>
-          <Link to="/contact">Contact</Link>
-          <a
-            href="https://wa.me/91XXXXXXXXXX"
-            target="_blank"
-            rel="noreferrer"
-          >
-            WhatsApp
-          </a>
-        </div>
-      </div>
+      <p className="profile">Abhishek</p>
     </div>
   );
 }
